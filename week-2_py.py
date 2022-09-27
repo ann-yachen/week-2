42354fd638964a34d60568cd37a5e7055f1828a3
ann-yachen <112312168+ann-yachen@users.noreply.github.com>
2022-09-28 07:43:16 +0800

    Update request-5
    Update request-5 for the correct answer

--- week-2_py.py
+++ week-2_py.py
@@ -80,7 +80,7 @@ def twoSum(nums, target):
     for i in range(0, len(nums)):
         for j in range(i+1, len(nums)):
             if nums[i] + nums [j] == target:
-                result = [nums[i], nums[j]]
+                result = [i, j]
                 break
     return result
 result = twoSum([2, 11, 7, 15], 9)
